9e9c2bbabd0d2eed8d7609bf47bd282c63613e93
nixonfdo <53013582+nixonfdo@users.noreply.github.com>
2019-07-23 09:13:27 -0700

    Create vowelconsonant.py

---
+++ vowelconsonant.py
@@ -0,0 +1,7 @@
+x=input()
+if (x=='!' or x=='@' or x=='%' or x=='$' or x=='^' or x=='&' or x=='*'):
+	print("invalid")
+elif (x=='a' or x=='e' or x=='i' or x=='o' or x=='u'):# your code goes here
+	print("Vowel")
+else:
+	print("Consonant")
